a11af8858fdce8135d0f9e5f5d9817744eb1b050
Smythe-100597078 <100597078@student.swin.edu.au>
2016-11-12 11:49:46 +1100

    Added SDL2 Library , SDL Image Library , and stdafx.h

---
+++ OccultusObfirmo/OccultusObfirmo/stdafx.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+// C++ Library headers
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+// SDL Libraries
+#include "SDL.h"
+#include "SDL_image.h"
+
+// Project Headers
